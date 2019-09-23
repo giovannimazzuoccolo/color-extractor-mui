7aa4323fccf4552b3f281fe727400f4d7da31ea5
Giovanni <gio.mazzuoccolo@gmail.com>
2019-09-23 22:10:01 +0200

    add colorpicker

--- src/themeInfo/ThemeInfo.tsx
+++ src/themeInfo/ThemeInfo.tsx
@@ -1,19 +1,30 @@
 import * as React from "react";
 import Typography from "@material-ui/core/Typography";
+import { ChromePicker } from 'react-color';
+import { Button } from "@material-ui/core";
 
 interface IProps {
     color: string;
   }
 
 const ThemeInfo: React.FunctionComponent<IProps> = (props: IProps) => {
+
+  const [ changeColorButton, changeColorButtonFunc ] = React.useState(false);
+
+
   return (
     <>
       <Typography variant="h5" color="primary">
         Theme info
       </Typography>
       <Typography>
-        Primary color: <strong>{props.color}</strong>
+      <Button onClick={(e) => {
+          changeColorButtonFunc(!changeColorButton)
+        }}>Change Color</Button> Primary color: <strong>{props.color}</strong> 
+        {changeColorButton && <ChromePicker color={props.color} />}
         <br />
+        Secondary color: <strong>{props.color}</strong>
+
       </Typography>
     </>
   );
