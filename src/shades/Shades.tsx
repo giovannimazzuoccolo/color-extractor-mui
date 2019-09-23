5fd453e11de2d92cb9dbcde387fca69a9cc4d8a3
Giovanni Mazzuoccolo <giovanni.mazzuoccolo@ke-works.com>
2019-09-23 17:54:03 +0200

    add menu part2

--- src/shades/Shades.tsx
+++ src/shades/Shades.tsx
@@ -1,29 +1,42 @@
 import * as React from "react";
 import Button from "@material-ui/core/Button";
 import Menu from "@material-ui/core/Menu";
+import MenuItem from '@material-ui/core/MenuItem';
+
 interface IProps {
   primaryColor: string;
   secondaryColor: string;
 }
 
-const Palette: React.FunctionComponent<IProps> = (props: IProps) => {
-  const SinglePalette = (props: { name: "primary" | "secondary" }) => {
-    const { name } = props;
-    return (
-      <Button color={name} variant="contained">
-        Change Shade
-      </Button>
-    );
-  };
+const SinglePalette = (props: { name: "primary" | "secondary" }) => {
+  const { name } = props;
+  return (
+    <Button color={name} variant="contained">
+      Change Shade
+    </Button>
+  );
+};
 
-  const Menu = (color:string) => {
-    
-  };
+const Palette: React.FunctionComponent<IProps> = (props: IProps) => {
+  const [isOpen, changeMenuState] = React.useState(false);
+
+  const PaletteMenu = (props: {color:string} ) => (
+    <Menu
+      id={`color_${props.color}`}
+      open={isOpen}
+      onClose={() => changeMenuState(false)}
+    >
+      <MenuItem>
+        Shade [10]
+      </MenuItem>
+    </Menu> 
+  );
 
   return (
     <div>
       <SinglePalette name="primary" />
       <SinglePalette name="secondary" />
+      <PaletteMenu color="#fff" />
     </div>
   );
 };
