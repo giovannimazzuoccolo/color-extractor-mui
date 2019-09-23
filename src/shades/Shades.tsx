5d8e8c6b938fa3fe4e6d0bf21aac6aafd6c0c377
Giovanni Mazzuoccolo <giovanni.mazzuoccolo@ke-works.com>
2019-09-23 17:48:39 +0200

    Added menu

--- src/shades/Shades.tsx
+++ src/shades/Shades.tsx
@@ -1,22 +1,29 @@
 import * as React from "react";
-import Button from '@material-ui/core/Button'
-
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
 interface IProps {
   primaryColor: string;
   secondaryColor: string;
 }
 
 const Palette: React.FunctionComponent<IProps> = (props: IProps) => {
-  const SinglePalette = (props: { name:"primary"|"secondary" }) => {
+  const SinglePalette = (props: { name: "primary" | "secondary" }) => {
     const { name } = props;
-    return <Button color={name} variant="contained" >
-    Change Shade</Button>;
+    return (
+      <Button color={name} variant="contained">
+        Change Shade
+      </Button>
+    );
+  };
+
+  const Menu = (color:string) => {
+    
   };
 
   return (
     <div>
       <SinglePalette name="primary" />
-      <SinglePalette name="secondary"/>
+      <SinglePalette name="secondary" />
     </div>
   );
 };
